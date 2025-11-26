2305f5abc97460bb2779d4fabb2b8a23a61c783a
uw-23-cy-bs-055-dotcom <uw-23-cy-bs-055@student.uow.edu.pk>
2025-11-26 10:30:26 +0500

    Refactor challenges initialization and setter function
    Refactor challenge key initialization and simplify function for setting blueprint challenge file.

--- data/datacache.ts
+++ data/datacache.ts
@@ -2,7 +2,6 @@
  * Copyright (c) 2014-2026 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
  */
-
 import type { BasketItemModel } from 'models/basketitem'
 import type { ChallengeKey, ChallengeModel } from 'models/challenge'
 import type { ComplaintModel } from 'models/complaint'
@@ -10,26 +9,14 @@ import type { FeedbackModel } from 'models/feedback'
 import type { ProductModel } from 'models/product'
 import type { BasketModel } from 'models/basket'
 import type { UserModel } from 'models/user'
-
 /* jslint node: true */
-
-export const challenges: Record<ChallengeKey, ChallengeModel> = {} as Record<ChallengeKey, ChallengeModel>
+export const challenges: Record<ChallengeKey, ChallengeModel> = {} as unknown as Record<ChallengeKey, ChallengeModel> // this is a hack to have the challenge key non-nullable, but on init it is null.
 export const users: Record<string, UserModel> = {}
 export const products: Record<string, ProductModel> = {}
-
-
-
-
-
-
-
-
-
 export const feedback: Record<string, FeedbackModel> = {}
 export const baskets: Record<string, BasketModel> = {}
 export const basketItems: Record<string, BasketItemModel> = {}
 export const complaints: Record<string, ComplaintModel> = {}
-
 export interface Notification {
   key: string
   name: string
@@ -38,18 +25,9 @@ export interface Notification {
   hidden: boolean
   isRestore: boolean
 }
-
 export const notifications: Notification[] = []
-
-// -----------------------------
-// FIX FOR MUTABLE EXPORT WARNING
-// -----------------------------
 let _retrieveBlueprintChallengeFile: string | null = null
 
-export function getRetrieveBlueprintChallengeFile(): string | null {
-  return _retrieveBlueprintChallengeFile
-}
-
-export function setRetrieveBlueprintChallengeFile(value: string): void {
-  _retrieveBlueprintChallengeFile = value
+export function setRetrieveBlueprintChallengeFile (retrieveBlueprintChallengeFileArg: string) {
+  retrieveBlueprintChallengeFile = retrieveBlueprintChallengeFileArg
 }
